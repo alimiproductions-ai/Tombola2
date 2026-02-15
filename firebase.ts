ed81f970f6d797890b369ea37c33af963be89d55
alimiproductions-ai <alimiproductions@gmail.com>
2026-02-15 15:50:26 +0200

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,6 @@
-
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-import { getAuth } from 'firebase/auth';
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZDCN7LTHrGacH-Y8mBStZ1eml5xeImo4",
@@ -9,6 +8,6 @@ const firebaseConfig = {
   databaseURL: "https://tombola-731c0-default-rtdb.firebaseio.com"
 };
 
+
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export const auth = getAuth(app);
+export const db = getFirestore(app); // C'est cette ligne qui est importante !
