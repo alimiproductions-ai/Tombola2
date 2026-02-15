190851fb0f6f500f0f85e721892d6a78fbf1e52a
alimiproductions-ai <alimiproductions@gmail.com>
2026-02-15 15:57:20 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // On importe le composant principal
-import './index.css'; // Si tu as un fichier CSS global (sinon supprime cette ligne)
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
