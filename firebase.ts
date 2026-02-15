1fdfe3523af048d8d75fed6aaa23b3f3827b9bcc
alimiproductions-ai <alimiproductions@gmail.com>
2026-02-15 15:52:41 +0200

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,13 +1,24 @@
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app); // C'est cette ligne qui est importante !
+
+
 // src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// --- REMPLACE CECI PAR TES PROPRES CLES FIREBASE ---
+// Tu les trouves dans la console Firebase > Paramètres du projet > Générale > Vos applications
 const firebaseConfig = {
   apiKey: "AIzaSyAZDCN7LTHrGacH-Y8mBStZ1eml5xeImo4",
   authDomain: "tombola-731c0.firebaseapp.com",
   databaseURL: "https://tombola-731c0-default-rtdb.firebaseio.com"
 };
+};
+// ---------------------------------------------------
 
-
+// 1. On initialise Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); // C'est cette ligne qui est importante !
+
+// 2. C'EST LA LIGNE MAGIQUE QUI MANQUAIT PEUT-ÊTRE :
+// On initialise la base de données (Firestore) et on l'exporte pour l'utiliser ailleurs
+export const db = getFirestore(app);
